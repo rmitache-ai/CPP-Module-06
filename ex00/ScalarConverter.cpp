d2f1457fd35f4e780ea3d1b787b12dcac6c2d1ef
rmitache <rmitache@student.42heilbronn.de>
2024-04-15 13:52:25 +0000

    Fix display_char function in ScalarConverter.cpp

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -13,16 +13,24 @@ ScalarConverter::~ScalarConverter() {
 }
 
 void ScalarConverter::display_char(std::string to_convert) {
+	if (to_convert == "nan") {
+		std::cout << "char: impossible" << std::endl;
+		return;
+	}
+	// // From 65 -> A
 	int new_char = std::atoi((to_convert).c_str());
-
-	// From A -> 65
 	if (new_char >= MIN_ASCII_VALUE
 		&& new_char <= MAX_ASCII_VALUE) {
 		_char = static_cast<char>(new_char);
 		std::cout << "char: " << _char << std::endl;
 		return;
 	}
-	// From 65 -> A
+	if (!(isdigit(std::atoi((to_convert).c_str())) != 0)
+		&& to_convert.length() == 1) {
+		_char = to_convert[0];
+		std::cout << "char: " << (int)_char << std::endl;
+		return;
+	}
 	std::cout << "char: Non displayable" << std::endl;
 }
 
